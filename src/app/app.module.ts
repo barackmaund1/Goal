d7325e1bb4238ddccc7984b10ea820c41e36377f
barackmaund1 <pmaundu49@gmail.co>
2020-03-26 14:05:27 +0300

    added all files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GoalComponent } from './goal/goal.component';
+import { StrikethroughDirective } from './strikethrough.directive';
+import { GoalDetailComponent } from './goal-detail/goal-detail.component';
+import { DateCountPipe } from './date-count.pipe';
+import { GoalFormComponent } from './goal-form/goal-form.component'
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoalComponent,
+    StrikethroughDirective,
+    GoalDetailComponent,
+    DateCountPipe,
+    GoalFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/strikethrough.directive.spec.ts
@@ -0,0 +1,8 @@
+import { StrikethroughDirective } from './strikethrough.directive';
+
+describe('StrikeDirective', () => {
+  it('should create an instance', () => {
+    const directive = new StrikethroughDirective();
+    expect(directive).toBeTruthy();
+  });
+});
